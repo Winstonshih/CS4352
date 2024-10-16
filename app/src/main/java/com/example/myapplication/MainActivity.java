fe9dc3bb8c41c6a275015f2751559aa7371017d9
Ruben Rodriguez <ruben13478@gmail.com>
2024-10-16 13:24:20 -0500

    Final Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+/* Ruben Rodriguez
+* rar180001
+* Completed*/
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    // these is our user and password that is already set
+EditText username;
+EditText password;
+Button loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //set the content that we are receiving from the user
+        setContentView(R.layout.activity_main);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        loginButton=findViewById(R.id.loginButton);
+        //when they click the button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if the username and password is correct
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivity.this, "Log in Successful!", Toast.LENGTH_SHORT).show();
+                    //go to the next page or activity
+                    Intent intent=new Intent(MainActivity.this,Input_Info_Activity.class);
+                    startActivity(intent);
+                    //there we go to the next page or better know as activity Input_Info_Activity
+                } else {
+                    //if the username and password is incorrect
+
+                    Toast.makeText(MainActivity.this, "Log in Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+    }
+}
