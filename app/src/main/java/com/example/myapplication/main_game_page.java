3158e91b602db8eddbea4abee3b6085faf89be01
Winstonshih <xxinfernobladexx@gmail.com>
2024-10-18 18:31:17 -0500

    Winston: Added input subscription pages with 3, 4, and 5 total additional subscriptions and linked subscription pages with 1 or 2 additional subscriptions to user_intro_display page.

--- app/src/main/java/com/example/myapplication/main_game_page.java
+++ app/src/main/java/com/example/myapplication/main_game_page.java
@@ -17,9 +17,9 @@ public class main_game_page extends AppCompatActivity {
         setContentView(R.layout.activity_main_game_page);
         RecyclerView recyclerView=findViewById(R.id.recyclerview);
         List<Item> items=new ArrayList<Item>();
-        items.add(new Item("Close a subscription", "Diamond helmet (+10 Protection)", R.drawable.upgradedhelmet));
-        items.add(new Item("Open a stock account", "Diamond Armor (+10 Protection)", R.drawable.upgradedarmor));
-        items.add(new Item("Open a savings account", "Diamond pants (+10 Protection)", R.drawable.upgradedpants));
+        items.add(new Item("Close a subscription.", "Diamond helmet (+10 Protection)", R.drawable.upgradedhelmet));
+        items.add(new Item("Make a Savings Account.", "Diamond Armor (+10 Protection)", R.drawable.upgradedarmor));
+        items.add(new Item("Add $20 to Savings Account.", "Diamond pants (+10 Protection)", R.drawable.upgradedpants));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new MyAdapter(getApplicationContext(), items));
     }
