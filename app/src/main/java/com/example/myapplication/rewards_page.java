f8c94eb753fae59ca5ff33380244a3adf9a1478c
SyedJibranShah <syedjshah1231@gmail.com>
2024-10-20 16:28:52 -0500

    Syed Shah work for stats page and xml

--- app/src/main/java/com/example/myapplication/rewards_page.java
+++ app/src/main/java/com/example/myapplication/rewards_page.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class rewards_page extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        // Home Button: Navigate to Main Game Page
+        Button homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_character_stats_page.this, main_game_page.class);
+                startActivity(intent);
+            }
         });
     }
 }
