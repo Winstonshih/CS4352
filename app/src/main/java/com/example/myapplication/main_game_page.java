33b1a88267be11a65d70a91cd27628b6aa898ad5
Ruben Rodriguez <ruben13478@gmail.com>
2024-10-26 20:35:36 -0500

    some changes to test

--- app/src/main/java/com/example/myapplication/main_game_page.java
+++ app/src/main/java/com/example/myapplication/main_game_page.java
@@ -8,106 +8,111 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-import java.util.List;
-import java.util.ArrayList;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-public class main_game_page extends AppCompatActivity{
-    ImageButton personImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main_game_page extends AppCompatActivity {
+    private ImageButton personImage;
     private ImageView helmet, chest, pants;
-    private MyAdapter adapter;
+    private RecyclerView recyclerView;
+    private SharedPreferences sharedTracker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_game_page);
-        RecyclerView recyclerView=findViewById(R.id.recyclerview);
-        helmet=findViewById(R.id.helmet);
-        chest=findViewById(R.id.chest);
-        pants=findViewById(R.id.pants);
-        //this is the sharable that keeps track of the items
-        SharedPreferences sharedTracker = getSharedPreferences("tracker", MODE_PRIVATE);
-        SharedPreferences.Editor trackerEditor = sharedTracker.edit();
-        //this is the sharable that keeps track of the equipment we have on
+
+        initializeViews();
+        loadEquipment();
+        setupRecyclerView();
+        setupButtons();
+    }
+
+    private void initializeViews() {
+        recyclerView = findViewById(R.id.recyclerview);
+        helmet = findViewById(R.id.helmet);
+        chest = findViewById(R.id.chest);
+        pants = findViewById(R.id.pants);
+        personImage = findViewById(R.id.personImage);
+    }
+
+    private void loadEquipment() {
+        sharedTracker = getSharedPreferences("tracker", MODE_PRIVATE);
+
         int helmetID = sharedTracker.getInt("helmet", 0);
         int chestID = sharedTracker.getInt("chest", 0);
         int pantsID = sharedTracker.getInt("pants", 0);
-        //this checks which helmet we have equipped
+
+        // Load helmet image
         if (helmetID == 1) {
             helmet.setImageResource(R.drawable.helmet);
-        }
-        else if (helmetID == 2) {
+        } else if (helmetID == 2) {
             helmet.setImageResource(R.drawable.upgradedhelmet);
         }
-        //this checks which chest we have equipped
+
+        // Load chest image
         if (chestID == 1) {
             chest.setImageResource(R.drawable.armor);
-        }
-        else if (chestID == 2) {
+        } else if (chestID == 2) {
             chest.setImageResource(R.drawable.upgradedarmor);
         }
-        //this checks which pants we have equipped
+
+        // Load pants image
         if (pantsID == 1) {
             pants.setImageResource(R.drawable.pants);
-        }
-        else if (pantsID == 2) {
+        } else if (pantsID == 2) {
             pants.setImageResource(R.drawable.upgradedpants);
         }
-        //end of checking which equipment we have equipped
+    }
 
-        List<Item> items=new ArrayList<Item>();
+    private void setupRecyclerView() {
+        List<Item> items = new ArrayList<>();
         items.add(new Item("Close a subscription", "Diamond helmet (+10 Protection)", R.drawable.upgradedhelmet, false));
         items.add(new Item("Make a Savings Account", "Diamond Armor (+10 Protection)", R.drawable.upgradedarmor, false));
         items.add(new Item("Add $20 to Savings Account", "Diamond pants (+10 Protection)", R.drawable.upgradedpants, false));
+
         MyAdapter adapter = new MyAdapter(this, items);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 
-
-
-
-        // Inventory Button: Navigate to Inventory Page
+    private void setupButtons() {
         Button inventoryButton = findViewById(R.id.inventoryButton);
+        Button rewardsButton = findViewById(R.id.rewardsButton);
+        Button homeButton = findViewById(R.id.homeButton);
+
         inventoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(main_game_page.this, inventory_page.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                startActivity(new Intent(main_game_page.this, inventory_page.class));
             }
         });
 
-        // Rewards Button: Navigate to Rewards Page
-        Button rewardsButton = findViewById(R.id.rewardsButton);
         rewardsButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(main_game_page.this, rewards_page.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                startActivity(new Intent(main_game_page.this, rewards_page.class));
             }
         });
 
-        // Home Button: Navigate to Main Game Page
-        Button homeButton = findViewById(R.id.homeButton);
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(main_game_page.this, main_game_page.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                startActivity(new Intent(main_game_page.this, main_game_page.class));
             }
         });
 
-        //Ruben Part when they click on the the image
-        personImage = (ImageButton) findViewById(R.id.personImage);
         personImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //go to the stats page
-                Intent intent = new Intent(main_game_page.this, main_character_stats_page.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                startActivity(new Intent(main_game_page.this, main_character_stats_page.class));
             }
         });
     }
-
 }
