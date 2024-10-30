8d31768c535e6df277c746e700e1b3c5593380fa
Winstonshih <xxinfernobladexx@gmail.com>
2024-10-29 22:37:21 -0500

    Winston: Added pop up functionality for when player presses unlock sword button to get sword.

--- app/src/main/java/com/example/myapplication/rewards_page.java
+++ app/src/main/java/com/example/myapplication/rewards_page.java
@@ -1,9 +1,15 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +18,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class rewards_page extends AppCompatActivity {
-
+Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,24 @@ public class rewards_page extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(rewards_page.this, main_game_page.class);
                 startActivity(intent);
+            }
+        });
+        LinearLayout rewardButton=findViewById(R.id.unlockableAward);
+        rewardButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                View popUpView = LayoutInflater.from(v.getContext()).inflate(R.layout.sword_popup, null);
+                PopupWindow popUpWindow = new PopupWindow(popUpView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+                popUpWindow.showAsDropDown(v);
+
+                Button swordClose = popUpView.findViewById(R.id.swordclose);
+                swordClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popUpWindow.dismiss();
+                    }
+                });
             }
         });
     }
