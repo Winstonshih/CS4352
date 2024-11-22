2e42ad24ba447fa77474cd94980c3f25f33acb1f
Winstonshih <xxinfernobladexx@gmail.com>
2024-11-22 17:35:47 -0600

    Winston: Added functionality to buttons on TaskHistory page (still need to remove tutorial functionality).

--- app/src/main/java/com/example/myapplication/TaskHistory.java
+++ app/src/main/java/com/example/myapplication/TaskHistory.java
@@ -1,9 +1,12 @@
 package com.example.myapplication;
 
 import android.app.Application;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,7 +35,7 @@ public class TaskHistory extends AppCompatActivity {
         setContentView(R.layout.activity_task_history);
 
         sharedPreferences = getSharedPreferences("GamePagePrefs", MODE_PRIVATE);
-
+        initializeButtons();
         initializeViews();
         setupRecyclerView();
     }
@@ -40,7 +43,29 @@ public class TaskHistory extends AppCompatActivity {
     private void initializeViews() {
         recyclerView = findViewById(R.id.recyclerview);
     }
-
+    private void initializeButtons()
+    {
+        Button inventoryButton = findViewById(R.id.inventoryButton);
+        Button rewardsButton = findViewById(R.id.rewardsButton);
+        Button homeButton = findViewById(R.id.homeButton);
+        Button editmenubutton = findViewById(R.id.editmenubutton);
+        ImageView personImage=findViewById(R.id.personImage);
+        inventoryButton.setOnClickListener(view ->
+                startActivity(new Intent(TaskHistory.this, inventory_page.class))
+        );
+        rewardsButton.setOnClickListener(view ->
+                startActivity(new Intent(TaskHistory.this, rewards_page.class))
+        );
+        homeButton.setOnClickListener(view ->
+                startActivity(new Intent(TaskHistory.this, main_game_page.class))
+        );
+        personImage.setOnClickListener(view ->
+                startActivity(new Intent(TaskHistory.this, main_character_stats_page.class))
+        );
+        editmenubutton.setOnClickListener(view ->
+                startActivity(new Intent(TaskHistory.this, Input_Info_Activity_2.class))
+        );
+    }
 
 
     private void setupRecyclerView() {
